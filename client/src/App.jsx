bd2575d6aa7403ff320e0ab9b11d753b7b2e87fd
Gary Rios <78942879+babypwant@users.noreply.github.com>
2023-06-28 16:25:01 -0700

    completed login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,16 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import IndexPage from './pages/IndexPage'
 import LoginPage from './pages/LoginPage'
+import Layout from './Layout'
 
 function App() {
 
   return (
     <Routes>
+      <Route path='/' element={<Layout/>}>
       <Route index element={<IndexPage/>} />
       <Route path='/login' element={<LoginPage/>} />
+      </Route>
     </Routes>
 
   )
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,6 +1,16 @@
 
 export default function LoginPage() {
   return (
-    <div>LoginPage</div>
+    <div className="mt-4 grow flex items-center justify-around">
+        <div className="mb-64">
+            <h1 className="text-4xl text-center mb-4">Login</h1>
+            <form className="max-w-md mx-auto ">
+                <input type='email' placeholder="your@email.com" />
+                <input type='password' placeholder="password" />
+                <button className="primary">Login</button>
+            </form>
+        </div>
+
+    </div>
   )
 }
