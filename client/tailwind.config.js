10d8b583b9d9f71406cc4d5209de158ae1fe906a
Gary Rios <78942879+babypwant@users.noreply.github.com>
2023-06-28 15:52:01 -0700

    header set up

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,11 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#F5385D'
+      }
+    },
   },
   plugins: [],
 }
