61efe2de569c55384572204f972b86db8096eea0
Gary Rios <78942879+babypwant@users.noreply.github.com>
2023-06-30 16:50:09 -0700

    Updates for production

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,8 +13,8 @@ import PlacePage from "./pages/PlacePage";
 import BookingsPage from "./pages/BookingsPage";
 import BookingPage from "./pages/BookingPage";
 
-// axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
-axios.defaults.baseURL = 'http://127.0.0.1:4000';
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
+// axios.defaults.baseURL = 'http://127.0.0.1:4000';
 
 axios.defaults.withCredentials = true;
 
