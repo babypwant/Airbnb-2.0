4eac41651c9d860a126a66ab658c4c86a085fed2
Gary Rios <78942879+babypwant@users.noreply.github.com>
2023-06-29 21:32:46 -0700

    connected radio buttons

--- client/src/Perks.jsx
+++ client/src/Perks.jsx
@@ -1,9 +1,22 @@
 
 export default function Perks({selected, onChange}) {
+
+  function handleCbClick(e){
+    const{checked, name} = e.target;
+    if(checked){
+      onChange([...selected, name])
+
+    }else{
+      onChange([...selected.filter(item => item !== name)])
+    }
+
+
+  }
+
   return (
     <>
         <label className='border p-4 flex rounded-2xl gap-2 items-center cursor-pointer' >
-                          <input type='checkbox' />
+                          <input name='TV' type='checkbox'  onChange={handleCbClick} />
                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125z" />
                           </svg>
@@ -12,7 +25,7 @@ export default function Perks({selected, onChange}) {
                       </label>
 
                       <label className='border p-4 flex rounded-2xl gap-2 items-center cursor-pointer' >
-                        <input type='checkbox' />
+                        <input name='wifi' type='checkbox'  onChange={handleCbClick}/>
                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M8.288 15.038a5.25 5.25 0 017.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 011.06 0z" />
                           </svg>
@@ -20,7 +33,7 @@ export default function Perks({selected, onChange}) {
                       </label>
 
                       <label className='border p-4 flex rounded-2xl gap-2 items-center cursor-pointer' >
-                          <input type='checkbox' />
+                          <input name='ev' type='checkbox'  onChange={handleCbClick}/>
                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M21 10.5h.375c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125H21M4.5 10.5H18V15H4.5v-4.5zM3.75 18h15A2.25 2.25 0 0021 15.75v-6a2.25 2.25 0 00-2.25-2.25h-15A2.25 2.25 0 001.5 9.75v6A2.25 2.25 0 003.75 18z" />
                           </svg>
@@ -28,7 +41,7 @@ export default function Perks({selected, onChange}) {
                       </label>
 
                       <label className='border p-4 flex rounded-2xl gap-2 items-center cursor-pointer' >
-                        <input type='checkbox' />
+                        <input name='pets' type='checkbox'  onChange={handleCbClick}/>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                           <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                         </svg>
@@ -37,7 +50,7 @@ export default function Perks({selected, onChange}) {
                       </label>
 
                       <label className='border p-4 flex rounded-2xl gap-2 items-center cursor-pointer' >
-                        <input type='checkbox' />
+                        <input name='ac' type='checkbox' onChange={handleCbClick} />
 
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                           <path strokeLinecap="round" strokeLinejoin="round" d="M9 9l6-6m0 0l6 6m-6-6v12a6 6 0 01-12 0v-3" />
@@ -47,7 +60,7 @@ export default function Perks({selected, onChange}) {
                       </label>
 
                       <label className='border p-4 flex rounded-2xl gap-2 items-center cursor-pointer' >
-                        <input type='checkbox' />
+                        <input name='kitchen' type='checkbox' onChange={handleCbClick} />
 
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                           <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
